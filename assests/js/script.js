9f0859c5d52f616dddcc3aece55e3f6cd9d16059
Arpita Tripathi <arpitatripathi182@gmail.com>
2026-01-04 14:22:09 +0530

    first cmm

--- assests/js/script.js
+++ assests/js/script.js
@@ -25,3 +25,27 @@ function submitForm() {
 // Dynamic year
 document.getElementById("year").innerHTML =
     new Date().getFullYear();
+
+    function validateForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let gender = document.getElementById("gender").value;
+    let msg = document.getElementById("msg");
+
+    if (name === "" || email === "" || password === "" || gender === "") {
+        msg.style.color = "red";
+        msg.innerHTML = "All fields are required!";
+        return false;
+    }
+
+    if (password.length < 6) {
+        msg.style.color = "red";
+        msg.innerHTML = "Password must be at least 6 characters!";
+        return false;
+    }
+
+    msg.style.color = "green";
+    msg.innerHTML = "Form submitted successfully!";
+    return false; // page reload stop
+}
